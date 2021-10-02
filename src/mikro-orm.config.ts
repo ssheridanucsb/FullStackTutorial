c8f0dd2b8d8fa59bc678f48720fd2054d939cc4a
Sam Sheridan <samsheridan@Sams-Air.lan>
2021-10-02 12:48:01 -0700

    created migrations

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,17 @@
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import { MikroORM } from "@mikro-orm/core";
+import path from 'path';
+
+export default {
+  migrations: {
+    path: path.join(__dirname, "./migrations"), // path to the folder with migrations
+    pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
+  },
+  entities: [Post],
+  dbName: "lireddit",
+  type: "postgresql",
+  user: "postgres",
+  password: "postgres",
+  debug: !__prod__,
+} as Parameters<typeof MikroORM.init>[0];
